da322e2a08755d93b042b1f4ccee5b8f146eb7e6
shankarbhattarai123 <shankarbhattarai913@gmail.com>
2021-05-21 18:53:48 +0545

    updated

--- mealapp/lib/main.dart
+++ mealapp/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Meal App',
+        title: 'Meals App',
         theme: ThemeData(
           primarySwatch: Colors.green,
           fontFamily: 'Cookie',
